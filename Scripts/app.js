be7f646c6fe177d916233c6b644b4b30c6df9f67
Javid <j-n_01@hotmail.com>
2017-11-19 10:42:02 +0400

    Added clients, contact, footer + css, js changes

---
+++ Scripts/app.js
@@ -0,0 +1,14 @@
+(function ($) {
+
+
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar-inverse").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+
+})(jQuery);
